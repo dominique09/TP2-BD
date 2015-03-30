f899ca104b85e9873b12d2fd0469c9a61497c95a
Charles-Édouard Beaudet <hhuhue@gmail.com>
2015-03-30 12:20:21 -0400

    Peuplement de PILOTE

---
+++ Inserts.sql
@@ -0,0 +1,64 @@
+/*===============================================================================
+     Table PILOTE : 4 Pilotes
+/*===============================================================================*/
+INSERT INTO
+	PILOTE
+		(ID_PILOTE,
+		NO_PILOTE,
+		NOM,
+		PRENOM,
+		ADRESSE,
+		TELEPHONE)
+	VALUES
+		(SEQ_ID_PILOTE.NEXTVAL,
+		22,
+		'LAVIGNE',
+		'ROGER',
+		'44 DUROCHER',
+		'418-666-3333');
+INSERT INTO
+	PILOTE
+		(ID_PILOTE,
+		NO_PILOTE,
+		NOM,
+		PRENOM,
+		ADRESSE,
+		TELEPHONE)
+	VALUES
+		(SEQ_ID_PILOTE.NEXTVAL,
+		34,
+		'LATRIMOUILLE',
+		'ANDRE',
+		'5353 MONFORT',
+		'540-555-3344');
+INSERT INTO
+	PILOTE
+		(ID_PILOTE,
+		NO_PILOTE,
+		NOM,
+		PRENOM,
+		ADRESSE,
+		TELEPHONE)
+	VALUES
+		(SEQ_ID_PILOTE.NEXTVAL,
+		55,
+		'LAVERTU',
+		'MARIE',
+		'5353 MONFORT',
+		'540-555-3344');
+INSERT INTO
+	PILOTE
+		(ID_PILOTE,
+		NO_PILOTE,
+		NOM,
+		PRENOM,
+		ADRESSE,
+		TELEPHONE)
+	VALUES
+		(SEQ_ID_PILOTE.NEXTVAL,
+		61,
+		'GENEST',
+		'CLAUDINE',
+		'1722 CRESENT, APP 3',
+		'514-555-1234');
+	
